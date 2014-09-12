cdbba031d656cf32e7675fbb34229a53832791be
Daniel Erat <dan@erat.org>
2014-09-11 19:25:48 -0700

    add tests for covers

---
+++ update_music_db/covers_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+)
+
+const (
+	testdataDir = "testdata/covers"
+)
+
+func TestBogusDir(t *testing.T) {
+	cf, err := newCoverFinder("bogus")
+	if cf != nil || err == nil {
+		t.Errorf("creation with bogus cover dir didn't fail")
+	}
+}
+
+func TestFindPath(t *testing.T) {
+	cf, err := newCoverFinder(testdataDir)
+	if err != nil {
+		t.Fatalf("creation failed")
+	}
+
+	for _, tc := range []struct{ artist, album, expected string }{
+		{"Artist", "Album", "Artist-Album.jpg"},
+		{"Foo", "Bar", "Foo-Bar.png"},
+		{"Pearl Jam", "Ten", "Pearl Jam-Ten.gif"},
+		{"AC/DC", "Back In Black", "AC%DC-Back In Black.jpg"},
+		{"Other Artist", "Album", "Artist-Album.jpg"},
+		{"Weird", "Band-We-Like-Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
+		{"Weird-Band", "We-Like-Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
+		{"Weird-Band-We", "Like-Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
+		{"Weird-Band-We-Like", "Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
+		{"Some Guy", "Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
+		{"Not There", "At All", ""},
+		{"Pearl Jam", "", ""},
+		{"", "", ""},
+	} {
+		actual := cf.findPath(tc.artist, tc.album)
+		if actual != tc.expected {
+			t.Errorf("%q, %q: expected %q but got %q", tc.artist, tc.album, tc.expected, actual)
+		}
+	}
+}
