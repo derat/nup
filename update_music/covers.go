93610f7af938e6939b6bb0fe56ec84493aaecb63
Daniel Erat <dan@erat.org>
2015-04-18 08:29:34 -0600

    update_music: improve cover selection with duplicate album names

--- update_music/covers.go
+++ update_music/covers.go
@@ -7,8 +7,10 @@ import (
 )
 
 type coverFinder struct {
+	// Artist-Album -> filename
 	artistAlbumMap map[string]string
-	albumMap       map[string]string
+	// Album -> Artist
+	albumMap map[string][]string
 }
 
 func newCoverFinder(coverDir string) (*coverFinder, error) {
@@ -19,7 +21,7 @@ func newCoverFinder(coverDir string) (*coverFinder, error) {
 
 	cf := &coverFinder{
 		artistAlbumMap: make(map[string]string),
-		albumMap:       make(map[string]string),
+		albumMap:       make(map[string][]string),
 	}
 	for _, fi := range entries {
 		fn := fi.Name()
@@ -39,9 +41,10 @@ func newCoverFinder(coverDir string) (*coverFinder, error) {
 		// following a hyphen to be a potential album.
 		cf.artistAlbumMap[base] = fn
 		for i := 1; i < len(parts); i++ {
+			artist := strings.Join(parts[:i], "-")
 			album := strings.Join(parts[i:], "-")
 			if len(album) > 0 {
-				cf.albumMap[strings.Join(parts[i:], "-")] = fn
+				cf.albumMap[album] = append(cf.albumMap[album], artist)
 			}
 		}
 	}
@@ -59,5 +62,32 @@ func (cf *coverFinder) findPath(artist, album string) string {
 	if fn, ok := cf.artistAlbumMap[artist+"-"+album]; ok {
 		return fn
 	}
-	return cf.albumMap[album]
+
+	// Return the filename corresponding to an artist in cf.albumMap.
+	artistFunc := func(ca string) string { return cf.artistAlbumMap[ca+"-"+album] }
+
+	// If we just have a single matching album, run with it.
+	coverArtists := cf.albumMap[album]
+	if len(coverArtists) == 1 {
+		return artistFunc(coverArtists[0])
+	}
+
+	// Match e.g. "[artist] feat. [someone else]".
+	for _, coverArtist := range coverArtists {
+		if strings.HasPrefix(artist, coverArtist) {
+			return artistFunc(coverArtist)
+		}
+	}
+
+	// Look for an album with a generic artist name.
+	var variousNames = [...]string{"Various Artists", "Various"}
+	for _, coverArtist := range coverArtists {
+		for _, various := range variousNames {
+			if coverArtist == various {
+				return artistFunc(coverArtist)
+			}
+		}
+	}
+
+	return ""
 }
--- update_music/covers_test.go
+++ update_music/covers_test.go
@@ -21,19 +21,25 @@ func TestCoversFindPath(t *testing.T) {
 	}
 
 	for _, tc := range []struct{ artist, album, expected string }{
-		{"Artist", "Album", "Artist-Album.jpg"},
-		{"Foo", "Bar", "Foo-Bar.png"},
-		{"Pearl Jam", "Ten", "Pearl Jam-Ten.gif"},
-		{"AC/DC", "Back In Black", "AC%DC-Back In Black.jpg"},
-		{"Other Artist", "Album", "Artist-Album.jpg"},
-		{"Weird", "Band-We-Like-Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
+		{"Artist", "Album", "Artist-Album.jpg"},                             // One album, exact match.
+		{"Foo", "Bar", "Foo-Bar.png"},                                       // Multiple albums, exact match.
+		{"Foo feat. Foobar", "Bar", "Foo-Bar.png"},                          // Multiple albums, one has artist prefix.
+		{"Baz", "Bar", "Baz-Bar.png"},                                       // Multiple albums, exact match.
+		{"Baz feat. Someone Else", "Bar", "Baz-Bar.png"},                    // Multiple albums, one has artist prefix.
+		{"Other", "Bar", ""},                                                // Multiple albums, no artist prefix.
+		{"Pearl Jam", "Ten", "Pearl Jam-Ten.gif"},                           // Space in artist name.
+		{"AC/DC", "Back In Black", "AC%DC-Back In Black.jpg"},               // Replace slashes.
+		{"Other Artist", "Album", "Artist-Album.jpg"},                       // No exact match, but only one album.
+		{"Coats", "Hats", "Coats-Hats.jpg"},                                 // Multiple albums, exact match.
+		{"Jackets", "Hats", "Various Artists-Hats.jpg"},                     // Multiple albums, one has generic artist.
+		{"Weird", "Band-We-Like-Hyphens", "Weird-Band-We-Like-Hyphens.jpg"}, // Split into all possible artist/album pairs.
 		{"Weird-Band", "We-Like-Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
 		{"Weird-Band-We", "Like-Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
 		{"Weird-Band-We-Like", "Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
 		{"Some Guy", "Hyphens", "Weird-Band-We-Like-Hyphens.jpg"},
-		{"Not There", "At All", ""},
-		{"Pearl Jam", "", ""},
-		{"", "", ""},
+		{"Not There", "At All", ""}, // No matches.
+		{"Pearl Jam", "", ""},       // Artist matches but album doesn't.
+		{"", "", ""},                // Missing artist/album.
 	} {
 		actual := cf.findPath(tc.artist, tc.album)
 		if actual != tc.expected {
