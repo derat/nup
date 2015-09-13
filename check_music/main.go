0f7f8fd63b157d8bd09a534c4dd61f69a6b09707
Daniel Erat <dan@erat.org>
2015-09-13 09:18:31 -0600

    check_music: refactor and check that covers are used

--- check_music/main.go
+++ check_music/main.go
@@ -13,13 +13,90 @@ import (
 	"erat.org/nup"
 )
 
-type checker func(s *nup.Song) error
+type songCheckFunction func(s *nup.Song) error
+type coverCheckFunction func(coverFilename string) error
 
 type songArray []*nup.Song
 
 func (a songArray) Len() int           { return len(a) }
 func (a songArray) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a songArray) Less(i, j int) bool { return a[i].Filename < a[j].Filename }
+
+func checkSongs(songs []*nup.Song, musicDir, coverDir string, strict bool) {
+	checkFile := func(s *nup.Song) error {
+		if len(s.Filename) == 0 {
+			return fmt.Errorf("no filename")
+		} else if _, err := os.Stat(filepath.Join(musicDir, s.Filename)); err != nil {
+			return fmt.Errorf("missing file %s", s.Filename)
+		}
+		return nil
+	}
+	checkAlbumInfo := func(s *nup.Song) error {
+		if strict && len(s.AlbumId) == 0 && s.Album != "[non-album tracks]" {
+			return fmt.Errorf("missing MusicBrainz album")
+		}
+		return nil
+	}
+	checkCover := func(s *nup.Song) error {
+		if len(s.CoverFilename) == 0 {
+			if strict {
+				return fmt.Errorf("no cover file")
+			} else {
+				return nil
+			}
+		} else if _, err := os.Stat(filepath.Join(coverDir, s.CoverFilename)); err != nil {
+			return fmt.Errorf("missing cover file %s", s.CoverFilename)
+		}
+		return nil
+	}
+	for _, c := range []songCheckFunction{
+		checkFile,
+		checkAlbumInfo,
+		checkCover,
+	} {
+		for _, s := range songs {
+			if err := c(s); err != nil {
+				log.Printf("%s (%s-%s): %v", s.SongId, s.Artist, s.Title, err)
+			}
+		}
+	}
+}
+
+func checkCovers(songs []*nup.Song, coverDir string) {
+	dir, err := os.Open(coverDir)
+	if err != nil {
+		log.Fatal("Failed to open cover dir: %v", err)
+	}
+	defer dir.Close()
+
+	coverFilenames, err := dir.Readdirnames(0)
+	if err != nil {
+		log.Fatal("Failed to read cover dir: %v", err)
+	}
+
+	songCoverFilenames := make(map[string]bool)
+	for _, s := range songs {
+		if len(s.CoverFilename) > 0 {
+			songCoverFilenames[s.CoverFilename] = true
+		}
+	}
+
+	checkUsed := func(coverFilename string) error {
+		if _, ok := songCoverFilenames[coverFilename]; !ok {
+			return fmt.Errorf("unused cover")
+		}
+		return nil
+	}
+	for _, c := range []coverCheckFunction{
+		checkUsed,
+	} {
+		for _, fn := range coverFilenames {
+			if err := c(fn); err != nil {
+				log.Printf("%s: %v", fn, err)
+			}
+		}
+	}
+}
 
 func main() {
 	musicDir := flag.String("music-dir", filepath.Join(os.Getenv("HOME"), "music"), "Directory containing music files")
@@ -46,44 +123,6 @@ func main() {
 
 	sort.Sort(songArray(songs))
 
-	checkMusicFile := func(s *nup.Song) error {
-		if len(s.Filename) == 0 {
-			return fmt.Errorf("no filename")
-		} else if _, err := os.Stat(filepath.Join(*musicDir, s.Filename)); err != nil {
-			return fmt.Errorf("missing file %s", s.Filename)
-		}
-		return nil
-	}
-	checkAlbumInfo := func(s *nup.Song) error {
-		if *strict && len(s.AlbumId) == 0 && s.Album != "[non-album tracks]" {
-			return fmt.Errorf("missing MusicBrainz album")
-		}
-		return nil
-	}
-	checkCover := func(s *nup.Song) error {
-		if len(s.CoverFilename) == 0 {
-			if *strict {
-				return fmt.Errorf("no cover file")
-			} else {
-				return nil
-			}
-		} else if _, err := os.Stat(filepath.Join(*coverDir, s.CoverFilename)); err != nil {
-			return fmt.Errorf("missing cover file %s", s.CoverFilename)
-		}
-		return nil
-	}
-
-	checkers := []checker{
-		checkMusicFile,
-		checkAlbumInfo,
-		checkCover,
-	}
-
-	for _, c := range checkers {
-		for _, s := range songs {
-			if err := c(s); err != nil {
-				log.Printf("%s (%s-%s): %v", s.SongId, s.Artist, s.Title, err)
-			}
-		}
-	}
+	checkSongs(songs, *musicDir, *coverDir, *strict)
+	checkCovers(songs, *coverDir)
 }
