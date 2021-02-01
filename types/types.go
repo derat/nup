c4285120f44723a7c976298c541e89614de1e450
Daniel Erat <dan@erat.org>
2021-02-01 19:53:19 -0400

    types: Update some comments to use godoc format.

--- types/types.go
+++ types/types.go
@@ -10,33 +10,33 @@ import (
 
 // Play represents one playback of a Song.
 type Play struct {
-	// Time at which playback started.
+	// StartTime is the time at which playback started.
 	StartTime time.Time `json:"t"`
-
-	// Client playing the song.
+	// IPAddress is the IPv4 or IPv6 address of the client playing the song.
 	IPAddress string `datastore:"IpAddress" json:"ip"`
 }
 
 // Song represents an audio file.
 type Song struct {
-	// SHA1 hash of the audio portion of the file.
+	// SHA1 is a hash of the audio portion of the file.
 	SHA1 string `datastore:"Sha1" json:"sha1,omitempty"`
 
-	// Song entity's key ID from Datastore. Only set in search results.
+	// SongID is the Song entity's key ID from Datastore. Only set in search results.
 	SongID string `datastore:"-" json:"songId,omitempty"`
 
-	// Relative path from the base of the music directory.
+	// Filename is a relative path from the base of the music directory.
 	// Must be escaped for Cloud Storage when constructing URL.
 	Filename string `json:"filename,omitempty"`
 
-	// Relative path from the base of the covers directory.
+	// CoverFilename is a relative path from the base of the covers directory.
 	// Must be escaped for Cloud Storage when constructing CoverURL.
 	// Clients can pass this to the server's /cover endpoint to get a scaled
 	// copy of the cover.
 	CoverFilename string `datastore:",noindex" json:"coverFilename,omitempty"`
 
-	// URL of the song and cover art. Only set in search results.
-	URL      string `datastore:"-" json:"url,omitempty"`
+	// URL is the song's URL. Only set in search results.
+	URL string `datastore:"-" json:"url,omitempty"`
+	// CoverURL is the cover image's URL. Only set in search results.
 	CoverURL string `datastore:"-" json:"coverUrl,omitempty"`
 
 	// Canonical versions used for display.
@@ -49,43 +49,45 @@ type Song struct {
 	TitleLower  string `json:"-"`
 	AlbumLower  string `json:"-"`
 
-	// Words from ArtistLower, TitleLower, and AlbumLower used for searching.
+	// Keywords contains words from ArtistLower, TitleLower, and AlbumLower used for searching.
 	Keywords []string `json:"-"`
 
-	// Opaque ID uniquely identifying the album (generally, a MusicBrainz
-	// release ID taken from a "MusicBrainz Album Id" ID3v2 tag).
+	// AlbumID is an opaque ID uniquely identifying the album
+	// (generally, a MusicBrainz release ID taken from a "MusicBrainz Album Id" ID3v2 tag).
 	AlbumID string `datastore:"AlbumId" json:"albumId,omitempty"`
 
-	// Opaque ID uniquely identifying the recording (generally, the MusicBrainz
-	// ID corresponding to the MusicBrainz recording entity, taken from a UFID
-	// ID3v2 tag). Only used to find cover art for non-album tracks.
+	// RecordingID is an opaque ID uniquely identifying the recording
+	// (generally, the MusicBrainz ID corresponding to the MusicBrainz recording entity,
+	// taken from a UFID ID3v2 tag). Only used to find cover art for non-album tracks.
 	RecordingID string `datastore:"-" json:"-"`
 
-	// Track and disc number or 0 if unset.
+	// Track is the song's track number, or 0 if unset.
 	Track int `json:"track"`
-	Disc  int `json:"disc"`
+	// Disc is the song's disc number, or 0 if unset.
+	Disc int `json:"disc"`
 
-	// Length in seconds.
+	// Length is the song's duration in seconds.
 	Length float64 `json:"length"`
 
-	// Rating in the range [0.0, 1.0] or -1 if unrated.
+	// Rating is the song's rating in the range [0.0, 1.0], or -1 if unrated.
 	Rating float64 `json:"rating"`
 
-	// First and last time the song was played.
+	// FirstStartTime is the first time the song was played.
 	FirstStartTime time.Time `json:"-"`
-	LastStartTime  time.Time `json:"-"`
+	// LastStartTime is the last time the song was played.
+	LastStartTime time.Time `json:"-"`
 
-	// Number of times the song has been played.
+	// NumPlays is the number of times the song has been played.
 	NumPlays int `json:"-"`
 
-	// The song's playback history.
+	// Plays contains the song's playback history.
 	// Only used for importing data -- in Datastore, Play is a descendant of Song.
 	Plays []Play `datastore:"-" json:"plays,omitempty"`
 
-	// Tags assigned to the song by the user.
+	// Tags contains tags assigned to the song by the user.
 	Tags []string `json:"tags"`
 
-	// Last time the song was modified.
+	// LastModifiedTime is the time that the song was modified.
 	LastModifiedTime time.Time `json:"-"`
 }
 
