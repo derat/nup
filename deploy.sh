f0b77f39102877eaf286ed97ae1cc96ee006f85f
Daniel Erat <dan@erat.org>
2021-03-01 11:20:48 -0400

    Move delete_old_versions.sh into deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,13 @@
 #!/bin/sh -e
-gcloud app --project=$(./project_id.sh) deploy "$@"
+
+project=$(./project_id.sh)
+gcloud app --project="$project" deploy "$@"
+
+# Clean up stale versions of the app so they aren't sitting around.
+versions=$(
+  gcloud app --project="$project" versions list |
+    sed -nre 's/^default +([^ ]+) +0\.00 .*/\1/p'
+)
+if [ -n "$versions" ]; then
+  gcloud app --project="$project" versions delete $versions
+fi
