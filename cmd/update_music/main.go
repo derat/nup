derat/nup

commit 9ddf61ebda4080169a07095bb7992fd51547260c

update_music: Fall back to 'nup Cover Id' user frame.
Make update_music try to get the cover art UUID from a 'nup
Cover Id' user frame if the 'MusicBrainz Album Id' frame
isn't found. This can be set on files that aren't in the
MusicBrainz database, e.g.

eyeD3 --user-text-frame='nup Cover Id:e9781b55-8e69-48ce-b15a-e8980326960a' song.mp3

cmd/update_music/scan.go
@@ -24,7 +24,8 @@ import (
 )
 
 const (
-	albumIDTag          = "MusicBrainz Album Id"
+	albumIDTag          = "MusicBrainz Album Id" // used as cover ID
+	coverIDTag          = "nup Cover Id"         // fallback if albumIDTag is unset
 	recordingIDOwner    = "http://musicbrainz.org"
 	logProgressInterval = 100
 	mp3Extension        = ".mp3"
@@ -225,10 +226,15 @@ func readSong(path, relPath string, fi os.FileInfo, gain *mp3gain.Info) (*types.
 		s.Artist = tag.Artist()
 		s.Title = tag.Title()
 		s.Album = tag.Album()
-		s.AlbumID = tag.CustomFrames()[albumIDTag]
 		s.RecordingID = tag.UniqueFileIdentifiers()[recordingIDOwner]
 		s.Track = int(tag.Track())
 		s.Disc = int(tag.Disc())
+
+		custom := tag.CustomFrames()
+		if s.AlbumID = custom[albumIDTag]; s.AlbumID == "" {
+			s.AlbumID = custom[coverIDTag]
+		}
+
 		headerLength = int64(tag.TagSize())
 	}
 

commit de64155f3d54d0e8b5460393faabb0eabbe57ca9

update_music: Don't use cover ID as album ID.
9ddf61ebda40 inadvertently made update_music set the Song
struct's AlbumID field from the 'nup Cover Id' tag. Add a
new CoverID field to Song so that the tag will only be used
when finding the cover image.

cmd/update_music/main.go
@@ -105,8 +105,10 @@ func main() {
 				log.Fatalf("Got error for %v: %v\n", s.Filename, s.Err)
 			}
 			s.CoverFilename = getCoverFilename(cfg.CoverDir, s.Song)
-			if *requireCovers && len(s.CoverFilename) == 0 && (len(s.AlbumID) > 0 || len(s.RecordingID) > 0) {
-				log.Fatalf("Failed to find cover for %v (album=%v, recording=%v)", s.Filename, s.AlbumID, s.RecordingID)
+			if *requireCovers && len(s.CoverFilename) == 0 &&
+				(len(s.AlbumID) > 0 || len(s.CoverID) > 0 || len(s.RecordingID) > 0) {
+				log.Fatalf("Failed to find cover for %v (album=%v, cover=%v, recording=%v)",
+					s.Filename, s.AlbumID, s.CoverID, s.RecordingID)
 			}
 			s.RecordingID = ""
 
@@ -164,16 +166,12 @@ func setLastUpdateTime(p string, t time.Time) error {
 
 // getCoverFilename returns the relative path under dir for song's cover image.
 func getCoverFilename(dir string, song *types.Song) string {
-	if len(song.AlbumID) != 0 {
-		fn := song.AlbumID + ".jpg"
-		if _, err := os.Stat(filepath.Join(dir, fn)); err == nil {
-			return fn
-		}
-	}
-	if len(song.RecordingID) != 0 {
-		fn := song.RecordingID + ".jpg"
-		if _, err := os.Stat(filepath.Join(dir, fn)); err == nil {
-			return fn
+	for _, s := range []string{song.CoverID, song.AlbumID, song.RecordingID} {
+		if len(s) > 0 {
+			fn := s + ".jpg"
+			if _, err := os.Stat(filepath.Join(dir, fn)); err == nil {
+				return fn
+			}
 		}
 	}
 	return ""

cmd/update_music/scan.go
@@ -24,8 +24,8 @@ import (
 )
 
 const (
-	albumIDTag          = "MusicBrainz Album Id" // used as cover ID
-	coverIDTag          = "nup Cover Id"         // fallback if albumIDTag is unset
+	albumIDTag          = "MusicBrainz Album Id" // usually used as cover ID
+	coverIDTag          = "nup Cover Id"         // can be set for non-MusicBrainz tracks
 	recordingIDOwner    = "http://musicbrainz.org"
 	logProgressInterval = 100
 	mp3Extension        = ".mp3"
@@ -226,15 +226,11 @@ func readSong(path, relPath string, fi os.FileInfo, gain *mp3gain.Info) (*types.
 		s.Artist = tag.Artist()
 		s.Title = tag.Title()
 		s.Album = tag.Album()
+		s.AlbumID = tag.CustomFrames()[albumIDTag]
+		s.CoverID = tag.CustomFrames()[coverIDTag]
 		s.RecordingID = tag.UniqueFileIdentifiers()[recordingIDOwner]
 		s.Track = int(tag.Track())
 		s.Disc = int(tag.Disc())
-
-		custom := tag.CustomFrames()
-		if s.AlbumID = custom[albumIDTag]; s.AlbumID == "" {
-			s.AlbumID = custom[coverIDTag]
-		}
-
 		headerLength = int64(tag.TagSize())
 	}
 

server/types/song.go
@@ -64,9 +64,14 @@ type Song struct {
 	// (generally, a MusicBrainz release ID taken from a "MusicBrainz Album Id" ID3v2 tag).
 	AlbumID string `datastore:"AlbumId" json:"albumId,omitempty"`
 
+	// CoverID is an opaque ID from a "nup Cover Id" ID3v2 tag used to specify cover art.
+	// If unset, AlbumID and then RecordingID is used when looking for art.
+	CoverID string `datastore:"-" json:"-"`
+
 	// RecordingID is an opaque ID uniquely identifying the recording
 	// (generally, the MusicBrainz ID corresponding to the MusicBrainz recording entity,
-	// taken from a UFID ID3v2 tag). Only used to find cover art for non-album tracks.
+	// taken from a UFID ID3v2 tag). Only used to find cover art if neither AlbumID nor CoverID
+	// is set.
 	RecordingID string `datastore:"-" json:"-"`
 
 	// Track is the song's track number, or 0 if unset.