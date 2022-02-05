5bf55a832d456b9c4366d642f88d585a24ca3c7e
Daniel Erat <dan@erat.org>
2022-02-05 13:07:01 -0400

    cmd/nup: Make -import-json-file preserve song order.
    Update 'nup update -import-json-file' send songs in the
    order in which they appear in the JSON file. Otherwise,
    -limit ends up sending arbitrary songs from the list.

--- cmd/nup/update/json.go
+++ cmd/nup/update/json.go
@@ -11,25 +11,37 @@ import (
 	"github.com/derat/nup/server/db"
 )
 
-// readSongsFromJSONFile JSON-unmarshals db.Song objects from path and sends
-// them to ch. The total number of sent songs is returned.
-func readSongsFromJSONFile(path string, ch chan songOrErr) (numSongs int, err error) {
+// readSongsFromJSONFile JSON-unmarshals db.Song objects from path and
+// asynchronously sends them to ch. The total number of songs is returned.
+func readSongsFromJSONFile(path string, ch chan songOrErr) (int, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return 0, err
 	}
 	defer f.Close()
 
+	// This interface is dumb. We need to return the total number of songs,
+	// but we also need to send the songs to the channel asynchronously to
+	// avoid blocking. Reading all the songs into memory seems like the
+	// best way to handle this. This function previously started a goroutine
+	// for each song, but that results in songs getting sent in an arbitrary
+	// order instead of the order in the file.
+	var songs []db.Song
 	d := json.NewDecoder(f)
 	for {
-		s := db.Song{}
+		var s db.Song
 		if err = d.Decode(&s); err == io.EOF {
 			break
 		} else if err != nil {
 			return 0, err
 		}
-		go func() { ch <- songOrErr{&s, nil} }()
-		numSongs++
+		songs = append(songs, s)
 	}
-	return numSongs, nil
+
+	go func() {
+		for i := range songs {
+			ch <- songOrErr{&songs[i], nil}
+		}
+	}()
+	return len(songs), nil
 }
