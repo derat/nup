d6fd2627177540c5c9047507627ed0ff34bf5a1d
Daniel Erat <dan@erat.org>
2022-04-09 12:56:13 -0400

    Add --indexes flag to deploy.sh.
    Make deploy.sh (slowly) update datastore indexes when -i or
    --indexes is passed rather than always updating them.

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,22 @@
 
 if [ "$1" = '-h' ] || [ "$1" = '--help' ]; then
   cat <<EOF >&2
-Usage: deploy.sh [args]...
+Usage: deploy.sh [flags]... [gcloud args]...
 Deploy the App Engine app to GCP and delete old versions.
+
+Flags:
+  -i, --indexes    Deploy datastore indexes rather than app
 EOF
   exit 2
 fi
 
 project=$(nup projectid)
+
+if [ "$1" = '-i' ] || [ "$1" = '--indexes' ]; then
+  gcloud beta datastore --project="$project" --quiet indexes create index.yaml
+  gcloud beta datastore --project="$project" --quiet indexes cleanup index.yaml
+  exit 0
+fi
 
 # As of November 2021, 'beta' is required here to use App Engine bundled
 # services (e.g. memcache) from the go115 runtime. Without it, the 'deploy'
@@ -19,9 +28,6 @@ project=$(nup projectid)
 # Surprisingly, --quiet only disables yes/no prompts (which we want) rather than
 # suppressing output (which we don't want).
 gcloud beta app --project="$project" --quiet deploy "$@"
-
-# Also update datastore indexes.
-gcloud beta datastore --project="$project" --quiet indexes create index.yaml
 
 # Clean up stale versions of the app so they aren't sitting around.
 versions=$(
