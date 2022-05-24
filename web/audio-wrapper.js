derat/nup

commit fc4ee66c5a306b56fa7feb9db54abb3cc9c0568a

web: Lots of miscellaneous cleanup.
Update web components to use connectedCallback() and
disconnectedCallback() to avoid leaking event listeners and
timeouts.

Also clean up other miscellaneous stuff, e.g. use === and
!==, includes() instead of indexOf(), optional chaining
(?.), nullish coalescing (??), etc.

web/audio-wrapper.js
@@ -62,6 +62,7 @@ customElements.define(
       this.gainNode_ = this.audioCtx_.createGain();
       this.gainNode_.connect(this.audioCtx_.destination);
       this.gain_ = 1;
+      this.audioSrc_ = null;
 
       this.shadow_ = createShadow(this, template);
       this.audio_ = this.shadow_.querySelector('audio');
@@ -75,20 +76,47 @@ customElements.define(
       this.pauseTimeoutId_ = null; // calls audio_.pause() after dropping gain
       this.pausedForOfflineTime_ = null; // seconds since epoch when auto-paused
       this.numErrors_ = 0; // consecutive playback errors
-
-      window.addEventListener('online', (e) => this.onOnline_(e));
     }
 
-    // Adds event handlers to |audio_| and routes it through |gainNode_|.
-    configureAudio_() {
-      this.audio_.addEventListener('ended', (e) => this.resendAudioEvent_(e));
-      this.audio_.addEventListener('error', (e) => this.onError_(e));
-      this.audio_.addEventListener('pause', (e) => this.onPause_(e));
-      this.audio_.addEventListener('play', (e) => this.onPlay_(e));
-      this.audio_.addEventListener('timeupdate', (e) => this.onTimeUpdate_(e));
+    connectedCallback() {
+      window.addEventListener('online', this.onOnline_);
+    }
 
+    disconnectedCallback() {
+      window.removeEventListener('online', this.onOnline_);
+
+      if (this.pauseTimeoutId_) {
+        window.clearTimeout(this.pauseTimeoutId_);
+        this.pauseTimeoutId_ = null;
+      }
+    }
+
+    // Adds event handlers to |audio_| and recreates |audioSrc_| to route
+    // playback through |gainNode_|.
+    configureAudio_() {
+      this.audio_.addEventListener('ended', this.onEnded_);
+      this.audio_.addEventListener('error', this.onError_);
+      this.audio_.addEventListener('pause', this.onPause_);
+      this.audio_.addEventListener('play', this.onPlay_);
+      this.audio_.addEventListener('timeupdate', this.onTimeUpdate_);
+
+      this.audioSrc_?.disconnect(this.gainNode_);
       this.audioSrc_ = this.audioCtx_.createMediaElementSource(this.audio_);
       this.audioSrc_.connect(this.gainNode_);
+    }
+
+    // Deconfigures |audio_| and replaces it with |audio|.
+    replaceAudio_(audio) {
+      this.audio_.removeAttribute('src');
+      this.audio_.removeEventListener('ended', this.onEnded_);
+      this.audio_.removeEventListener('error', this.onError_);
+      this.audio_.removeEventListener('pause', this.onPause_);
+      this.audio_.removeEventListener('play', this.onPlay_);
+      this.audio_.removeEventListener('timeupdate', this.onTimeUpdate_);
+
+      this.audio_.parentNode.replaceChild(audio, this.audio_);
+      this.audio_ = audio;
+      this.configureAudio_();
     }
 
     onOnline_(e) {
@@ -104,7 +132,11 @@ customElements.define(
       }
     }
 
-    onError_(e) {
+    onEnded_ = (e) => {
+      this.resendAudioEvent_(e);
+    };
+
+    onError_ = (e) => {
       if (e.target !== this.audio_) return;
 
       this.numErrors_++;
@@ -131,19 +163,19 @@ customElements.define(
           }
           break;
       }
-    }
+    };
 
-    onPause_(e) {
+    onPause_ = (e) => {
       this.lastUpdateTime_ = null;
       this.resendAudioEvent_(e);
-    }
+    };
 
-    onPlay_(e) {
+    onPlay_ = (e) => {
       this.lastUpdateTime_ = getCurrentTimeSec();
       this.resendAudioEvent_(e);
-    }
+    };
 
-    onTimeUpdate_(e) {
+    onTimeUpdate_ = (e) => {
       if (e.target !== this.audio_) return;
 
       const pos = this.audio_.currentTime;
@@ -164,7 +196,7 @@ customElements.define(
       this.numErrors_ = 0;
 
       this.resendAudioEvent_(e);
-    }
+    };
 
     // Dispatches a new event based on |e|.
     resendAudioEvent_(e) {
@@ -222,12 +254,8 @@ customElements.define(
         this.audio_.pause();
         this.audio_.removeAttribute('src');
       } else if (this.preloadSrc === src && !this.preloadAudio_.error) {
-        this.audioSrc_.disconnect(this.gainNode_);
-        this.audio_.removeAttribute('src');
-        this.audio_.parentNode.replaceChild(this.preloadAudio_, this.audio_);
-        this.audio_ = this.preloadAudio_;
+        this.replaceAudio_(this.preloadAudio_);
         this.preloadAudio_ = null;
-        this.configureAudio_(); // resets |audioSrc_|
       } else {
         this.audio_.src = src;
       }

test/web/unit/updater.test.js
@@ -7,13 +7,21 @@ import Updater from './updater.js';
 
 suite('updater', () => {
   let w = null;
+  let updater = null;
+
   beforeEach(() => {
     w = new MockWindow();
   });
   afterEach(() => {
+    updater?.destroy();
+    updater = null;
     w.finish();
   });
 
+  function initUpdater() {
+    updater?.destroy();
+    updater = new Updater();
+  }
   function playedUrl(songId, startTime) {
     return `played?songId=${songId}&startTime=${startTime}`;
   }
@@ -25,14 +33,14 @@ suite('updater', () => {
   }
 
   test('reportPlay (success)', async () => {
-    const updater = new Updater();
+    initUpdater();
     w.expectFetch(playedUrl('123', 456.5), 'POST', 'ok');
     await updater.reportPlay('123', 456.5);
     expectEq(w.numTimeouts, 0, 'numTimeouts');
   });
 
   test('reportPlay (retry)', async () => {
-    const updater = new Updater();
+    initUpdater();
 
     // Report a play and have the server return a 500 error.
     const id1 = '123';
@@ -59,7 +67,7 @@ suite('updater', () => {
 
   test('reportPlay (backoff)', async () => {
     // Make the initial attempt fail.
-    const updater = new Updater();
+    initUpdater();
     w.expectFetch(playedUrl('1', 2), 'POST', 'whoops', 500);
     await updater.reportPlay('1', 2);
 
@@ -92,7 +100,7 @@ suite('updater', () => {
   test('reportPlay (retry at startup)', async () => {
     // Make a playback report fail.
     const id = '1';
-    let updater = new Updater();
+    initUpdater();
     w.expectFetch(playedUrl(id, 1), 'POST', 'fail', 500);
     await updater.reportPlay(id, 1);
 
@@ -107,14 +115,14 @@ suite('updater', () => {
     // gets moved to the end of the queue this time).
     w.clearTimeouts();
     w.expectFetch(playedUrl(id, 1), 'POST', 'fail', 500);
-    updater = new Updater();
+    initUpdater();
     await updater.initialRetryDoneForTest;
 
     // After creating another updater, it should send the second report first
     // and then the first one.
     w.clearTimeouts();
     w.expectFetch(playedUrl(id, 2), 'POST', 'ok');
-    updater = new Updater();
+    initUpdater();
     await updater.initialRetryDoneForTest;
 
     w.expectFetch(playedUrl(id, 1), 'POST', 'ok');
@@ -122,7 +130,7 @@ suite('updater', () => {
     expectEq(w.numTimeouts, 0, 'numTimeouts');
 
     // If we create a new updater again, nothing should be sent.
-    updater = new Updater();
+    initUpdater();
     await updater.initialRetryDoneForTest;
     expectEq(w.numTimeouts, 0, 'numTimeouts');
   });
@@ -130,7 +138,7 @@ suite('updater', () => {
   test('reportPlay (overlapping)', async () => {
     // Report a play, but leave the fetch() call hanging.
     const id = '1';
-    let updater = new Updater();
+    initUpdater();
     const finishFetch = w.expectFetchDeferred(playedUrl(id, 1), 'POST', 'ok');
     const reportDone = updater.reportPlay(id, 1);
 
@@ -144,7 +152,7 @@ suite('updater', () => {
     expectEq(w.numTimeouts, 0, 'numTimeouts');
 
     // If we create a new updater, nothing should be sent.
-    updater = new Updater();
+    initUpdater();
     await updater.initialRetryDoneForTest;
     expectEq(w.numTimeouts, 0, 'numTimeouts');
   });
@@ -152,7 +160,7 @@ suite('updater', () => {
   test('reportPlay (online/offline)', async () => {
     // Make the initial attempt file.
     const id = '1';
-    let updater = new Updater();
+    initUpdater();
     w.expectFetch(playedUrl(id, 1), 'POST', 'fail', 500);
     await updater.reportPlay(id, 1);
 
@@ -170,7 +178,7 @@ suite('updater', () => {
   });
 
   test('rateAndTag (success)', async () => {
-    const updater = new Updater();
+    initUpdater();
     w.expectFetch(rateAndTagUrl('123', 4, null), 'POST', 'ok');
     await updater.rateAndTag('123', 4, null);
     w.expectFetch(rateAndTagUrl('123', null, ['abc', 'def']), 'POST', 'ok');
@@ -181,7 +189,7 @@ suite('updater', () => {
   });
 
   test('rateAndTag (retry)', async () => {
-    const updater = new Updater();
+    initUpdater();
 
     // Rate and tag a song and have the server report failure.
     w.expectFetch(rateAndTagUrl('123', 2, ['old']), 'POST', 'bad', 500);
@@ -204,7 +212,7 @@ suite('updater', () => {
 
   test('rateAndTag (retry at startup)', async () => {
     // Make the initial attempt fail.
-    let updater = new Updater();
+    initUpdater();
     w.expectFetch(rateAndTagUrl('123', 5, ['tag']), 'POST', 'bad', 500);
     await updater.rateAndTag('123', 5, ['tag']);
 
@@ -216,20 +224,20 @@ suite('updater', () => {
     // Fail again with a new updater.
     w.clearTimeouts();
     w.expectFetch(rateAndTagUrl('123', 5, ['tag']), 'POST', 'bad', 500);
-    updater = new Updater();
+    initUpdater();
     await updater.initialRetryDoneForTest;
 
     // Create another updater and let both updates get sent successfully.
     w.clearTimeouts();
     w.expectFetch(rateAndTagUrl('123', 5, ['tag']), 'POST', 'ok');
     w.expectFetch(rateAndTagUrl('456', 1, ['a']), 'POST', 'ok');
-    updater = new Updater();
+    initUpdater();
     await updater.initialRetryDoneForTest;
     await w.runTimeouts(0);
     expectEq(w.numTimeouts, 0, 'numTimeouts');
 
     // If we create a new updater again, nothing should be sent.
-    updater = new Updater();
+    initUpdater();
     await updater.initialRetryDoneForTest;
     expectEq(w.numTimeouts, 0, 'numTimeouts');
   });

web/common.js
@@ -7,7 +7,7 @@ export const emptyImg =
 
 // Returns the element under |root| with ID |id|.
 export function $(id, root) {
-  return (root || document).getElementById(id);
+  return (root ?? document).getElementById(id);
 }
 
 // Clamps number |val| between |min| and |max|.

web/index.js
@@ -85,8 +85,8 @@ document.test = {
     const srcX = srcRect.x + srcRect.width / 2;
     const srcY = srcRect.y + srcRect.height / 2;
     const destRect = dest.getBoundingClientRect();
-    const destX = destRect.x + destRect.width / 2 + (offsetX || 0);
-    const destY = destRect.y + destRect.height / 2 + (offsetY || 0);
+    const destX = destRect.x + destRect.width / 2 + (offsetX ?? 0);
+    const destY = destRect.y + destRect.height / 2 + (offsetY ?? 0);
 
     src.dispatchEvent(makeEvent('dragstart', srcX, srcY));
     dest.dispatchEvent(makeEvent('dragenter', destX, destY));

web/menu.js
@@ -82,7 +82,7 @@ export function createMenu(x, y, items, alignRight) {
     } else {
       const el = createElement('div', 'item', shadow, item.text);
       if (item.id) el.id = item.id;
-      if (hotkeys) createElement('span', 'hotkey', el, item.hotkey || '');
+      if (hotkeys) createElement('span', 'hotkey', el, item.hotkey ?? '');
       el.addEventListener('click', (e) => {
         e.stopPropagation();
         menu.close();

web/music-player.js
@@ -233,8 +233,7 @@ customElements.define(
     constructor() {
       super();
 
-      this.updater_ = new Updater();
-
+      this.updater_ = null; // initialized in connectedCallback()
       this.songs_ = []; // songs in the order in which they should be played
       this.tags_ = []; // available tags loaded from server
       this.currentIndex_ = -1; // index into |songs| of current track
@@ -246,7 +245,7 @@ customElements.define(
       this.notification_ = null; // song notification currently shown
       this.closeNotificationTimeoutId_ = null; // for closeNotification_()
       this.playDelayMs_ = this.constructor.PLAY_DELAY_MS_;
-      this.playTimeoutId_ = 0; // for playInternal_()
+      this.playTimeoutId_ = null; // for playInternal_()
       this.shuffled_ = false; // playlist contains shuffled songs
 
       this.shadow_ = createShadow(this, template);
@@ -408,6 +407,13 @@ customElements.define(
         });
       });
 
+      this.updateTagsFromServer_();
+      this.updateSongDisplay_();
+    }
+
+    connectedCallback() {
+      this.updater_ = new Updater();
+
       if ('mediaSession' in navigator) {
         const ms = navigator.mediaSession;
         ms.setActionHandler('play', () => this.play_(false /* delay */));
@@ -426,23 +432,40 @@ customElements.define(
         );
       }
 
-      document.body.addEventListener('keydown', (e) => {
-        if (this.processAccelerator_(e)) {
-          e.preventDefault();
-          e.stopPropagation();
-        }
-      });
-      window.addEventListener('beforeunload', () => {
-        this.closeNotification_();
-        return null;
-      });
+      document.body.addEventListener('keydown', this.onKeyDown_);
+      window.addEventListener('beforeunload', this.onBeforeUnload_);
+    }
 
-      this.updateTagsFromServer_();
-      this.updateSongDisplay_();
+    disconnectedCallback() {
+      this.updater_.destroy();
+      this.updater_ = null;
+
+      if (this.closeNotificationTimeoutId_) {
+        window.clearTimeout(this.closeNotificationTimeoutId_);
+        this.closeNotificationTimeoutId_ = null;
+      }
+      if (this.playTimeoutId_) {
+        window.clearTimeout(this.playTimeoutId_);
+        this.playTimeoutId_ = null;
+      }
+
+      if ('mediaSession' in navigator) {
+        const ms = navigator.mediaSession;
+        ms.setActionHandler('play', null);
+        ms.setActionHandler('pause', null);
+        ms.setActionHandler('seekbackward', null);
+        ms.setActionHandler('seekforward', null);
+        ms.setActionHandler('previoustrack', null);
+        ms.setActionHandler('nexttrack', null);
+      }
+
+      document.body.removeEventListener('keydown', this.onKeyDown_);
+      window.removeEventListener('beforeunload', this.onBeforeUnload_);
     }
 
     set config(config) {
       this.config_ = config;
+      // TODO: Remove callback in disconnectedCallback().
       this.config_.addCallback((name, value) => {
         if (name === Config.GAIN_TYPE || name === Config.PRE_AMP) {
           this.updateGain_();
@@ -454,6 +477,18 @@ customElements.define(
     get updateDivShown() {
       return !!this.updateSong_;
     }
+
+    onKeyDown_ = (e) => {
+      if (this.processAccelerator_(e)) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    };
+
+    onBeforeUnload_ = () => {
+      this.closeNotification_();
+      return null;
+    };
 
     // Requests known tags from the server and updates the internal list.
     // Returns a promise for completion of the task.
@@ -471,10 +506,10 @@ customElements.define(
     }
 
     get currentSong_() {
-      return this.songs_[this.currentIndex_] || null;
+      return this.songs_[this.currentIndex_] ?? null;
     }
     get nextSong_() {
-      return this.songs_[this.currentIndex_ + 1] || null;
+      return this.songs_[this.currentIndex_ + 1] ?? null;
     }
 
     resetForTesting() {
@@ -638,7 +673,7 @@ customElements.define(
       // Note that this will probably only work for non-admin users due to an
       // App Engine "feature": https://github.com/derat/nup/issues/1
       const precacheCover = (s) => {
-        if (!s || !s.coverFilename) return;
+        if (!s?.coverFilename) return;
         new Image().src = getScaledCoverUrl(s.coverFilename, smallCoverSize);
       };
       precacheCover(this.songs_[this.currentIndex_ + 1]);
@@ -702,7 +737,7 @@ customElements.define(
     updatePresentationLayerSongs_() {
       this.presentationLayer_.updateSongs(
         this.currentSong_,
-        this.songs_[this.currentIndex_ + 1] || null
+        this.songs_[this.currentIndex_ + 1] ?? null
       );
     }
 
@@ -739,7 +774,7 @@ customElements.define(
     }
 
     closeNotification_() {
-      if (this.notification_) this.notification_.close();
+      this.notification_?.close();
       this.notification_ = null;
     }
 
@@ -776,7 +811,7 @@ customElements.define(
       // Get an absolute URL since that's what we'll get from the <audio>
       // element: https://stackoverflow.com/a/44547904
       const url = getSongUrl(song.filename);
-      if (this.audio_.src != url || this.reachedEndOfSongs_) {
+      if (this.audio_.src !== url || this.reachedEndOfSongs_) {
         console.log(`Starting ${song.songId} (${url})`);
         this.audio_.src = url;
         this.audio_.currentTime = 0;
@@ -796,7 +831,7 @@ customElements.define(
         // errors.
         // https://developers.google.com/web/updates/2017/06/play-request-was-interrupted
         if (
-          e.name == 'AbortError' &&
+          e.name === 'AbortError' &&
           (e.message.match(/interrupted by a new load request/) ||
             e.message.match(/interrupted by a call to pause/))
         ) {
@@ -903,7 +938,7 @@ customElements.define(
 
       if (!song || !saveChanges) return;
 
-      const ratingChanged = this.updatedRating_ != song.rating;
+      const ratingChanged = this.updatedRating_ !== song.rating;
 
       const newRawTags = this.tagsTextarea_.value.trim().split(/\s+/);
       let newTags = [];
@@ -911,12 +946,12 @@ customElements.define(
       for (let i = 0; i < newRawTags.length; ++i) {
         let tag = newRawTags[i].toLowerCase();
         if (tag === '') continue;
-        if (this.tags_.indexOf(tag) != -1 || song.tags.indexOf(tag) != -1) {
+        if (this.tags_.includes(tag) || song.tags.includes(tag)) {
           newTags.push(tag);
-        } else if (tag[0] == '+' && tag.length > 1) {
+        } else if (tag[0] === '+' && tag.length > 1) {
           tag = tag.substring(1);
           newTags.push(tag);
-          if (this.tags_.indexOf(tag) == -1) createdTags.push(tag);
+          if (!this.tags_.includes(tag)) createdTags.push(tag);
         } else {
           console.log(`Skipping unknown tag "${tag}"`);
         }
@@ -924,8 +959,8 @@ customElements.define(
       // Remove duplicates.
       newTags = newTags
         .sort()
-        .filter((item, pos, self) => self.indexOf(item) == pos);
-      const tagsChanged = newTags.join(' ') != song.tags.sort().join(' ');
+        .filter((item, pos, self) => self.indexOf(item) === pos);
+      const tagsChanged = newTags.join(' ') !== song.tags.sort().join(' ');
 
       if (createdTags.length > 0) {
         this.updateTags_(this.tags_.concat(createdTags));
@@ -971,7 +1006,7 @@ customElements.define(
 
     // Shows or hides the presentation layer.
     setPresentationLayerVisible_(visible) {
-      if (this.presentationLayer_.visible == visible) return;
+      if (this.presentationLayer_.visible === visible) return;
       this.presentationLayer_.visible = visible;
       this.dispatchEvent(new CustomEvent('present', { detail: { visible } }));
     }
@@ -990,9 +1025,9 @@ customElements.define(
         }
 
         if (gainType === Config.GAIN_ALBUM) {
-          adj += song.albumGain || 0;
+          adj += song.albumGain ?? 0;
         } else if (gainType === Config.GAIN_TRACK) {
-          adj += song.trackGain || 0;
+          adj += song.trackGain ?? 0;
         }
       }
 
@@ -1010,54 +1045,54 @@ customElements.define(
     processAccelerator_(e) {
       if (isDialogShown() || isMenuShown()) return false;
 
-      if (e.altKey && e.key == 'd') {
+      if (e.altKey && e.key === 'd') {
         const song = this.currentSong_;
         if (song) window.open(getDumpSongUrl(song.songId), '_blank');
         this.setPresentationLayerVisible_(false);
         return true;
-      } else if (e.altKey && e.key == 'i') {
+      } else if (e.altKey && e.key === 'i') {
         const song = this.currentSong_;
         if (song) showSongInfo(song);
         this.setPresentationLayerVisible_(false);
         return true;
-      } else if (e.altKey && e.key == 'n') {
+      } else if (e.altKey && e.key === 'n') {
         this.cycleTrack_(1, true /* delay */);
         return true;
-      } else if (e.altKey && e.key == 'o') {
+      } else if (e.altKey && e.key === 'o') {
         showOptionsDialog(this.config_);
         this.setPresentationLayerVisible_(false);
         return true;
-      } else if (e.altKey && e.key == 'p') {
+      } else if (e.altKey && e.key === 'p') {
         this.cycleTrack_(-1, true /* delay */);
         return true;
-      } else if (e.altKey && e.key == 'r') {
+      } else if (e.altKey && e.key === 'r') {
         if (this.showUpdateDiv_()) this.ratingSpan_.focus();
         this.setPresentationLayerVisible_(false);
         return true;
-      } else if (e.altKey && e.key == 't') {
+      } else if (e.altKey && e.key === 't') {
         if (this.showUpdateDiv_()) this.tagsTextarea_.focus();
         this.setPresentationLayerVisible_(false);
         return true;
-      } else if (e.altKey && e.key == 'v') {
+      } else if (e.altKey && e.key === 'v') {
         this.setPresentationLayerVisible_(!this.presentationLayer_.visible);
         if (this.updateSong_) this.hideUpdateDiv_(false);
         return true;
-      } else if (e.key == ' ' && !this.updateSong_) {
+      } else if (e.key === ' ' && !this.updateSong_) {
         this.togglePause_();
         return true;
-      } else if (e.key == 'Enter' && this.updateSong_) {
+      } else if (e.key === 'Enter' && this.updateSong_) {
         this.hideUpdateDiv_(true);
         return true;
-      } else if (e.key == 'Escape' && this.presentationLayer_.visible) {
+      } else if (e.key === 'Escape' && this.presentationLayer_.visible) {
         this.setPresentationLayerVisible_(false);
         return true;
-      } else if (e.key == 'Escape' && this.updateSong_) {
+      } else if (e.key === 'Escape' && this.updateSong_) {
         this.hideUpdateDiv_(false);
         return true;
-      } else if (e.key == 'ArrowLeft' && !this.updateSong_) {
+      } else if (e.key === 'ArrowLeft' && !this.updateSong_) {
         this.seek_(-this.constructor.SEEK_SEC_);
         return true;
-      } else if (e.key == 'ArrowRight' && !this.updateSong_) {
+      } else if (e.key === 'ArrowRight' && !this.updateSong_) {
         this.seek_(this.constructor.SEEK_SEC_);
         return true;
       }
@@ -1066,12 +1101,12 @@ customElements.define(
     }
 
     handleRatingSpanKeyDown_(e) {
-      if (['0', '1', '2', '3', '4', '5'].indexOf(e.key) != -1) {
+      if (['0', '1', '2', '3', '4', '5'].includes(e.key)) {
         this.setRating_(parseInt(e.key));
         e.preventDefault();
         e.stopPropagation();
-      } else if (e.key == 'ArrowLeft' || e.key == 'ArrowRight') {
-        const rating = this.updatedRating_ + (e.key == 'ArrowLeft' ? -1 : 1);
+      } else if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+        const rating = this.updatedRating_ + (e.key === 'ArrowLeft' ? -1 : 1);
         this.setRating_(clamp(rating, 0, 5));
         e.preventDefault();
         e.stopPropagation();

commit 9ed05538ef89df66a3eedd17692a960be9dd960b

web: Fix audio-wrapper 'online' handler.
I think I broke this in fc4ee66c.

web/audio-wrapper.js
@@ -119,7 +119,7 @@ customElements.define(
       this.configureAudio_();
     }
 
-    onOnline_(e) {
+    onOnline_ = (e) => {
       // Automatically resume playing if we previously paused due to going
       // offline: https://github.com/derat/nup/issues/17
       if (this.pausedForOfflineTime_ !== null) {
@@ -130,7 +130,7 @@ customElements.define(
         this.reloadAudio_();
         if (resume) this.audio_.play();
       }
-    }
+    };
 
     onEnded_ = (e) => {
       this.resendAudioEvent_(e);