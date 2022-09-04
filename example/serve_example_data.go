derat/nup

commit 7a893b915902f3b87401e1f7c1280f554b9b5648

test: Move songs from test/data/music to test/data/songs.

example/serve_example_data.go
@@ -74,7 +74,7 @@ func run() (int, error) {
 	cfg := &config.Config{
 		GoogleUsers:    []string{*email},
 		BasicAuthUsers: []config.BasicAuthInfo{{Username: test.Username, Password: test.Password}},
-		SongBaseURL:    fileSrv.URL + "/music/",
+		SongBaseURL:    fileSrv.URL + "/songs/",
 		CoverBaseURL:   fileSrv.URL + "/covers/",
 		Presets:        presets,
 		Minify:         minify,

test/lib.go
@@ -33,7 +33,7 @@ func CopySongs(dir string, filenames ...string) error {
 	if err != nil {
 		return err
 	}
-	musicDir := filepath.Join(libDir, "data/music")
+	musicDir := filepath.Join(libDir, "data/songs")
 
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		return err

commit 3cd9e657e42e3f809016609162b016a3cc40a203

example: Fix music path broken by 7a893b915902.

example/serve_example_data.go
@@ -74,7 +74,7 @@ func run() (int, error) {
 	cfg := &config.Config{
 		GoogleUsers:    []string{*email},
 		BasicAuthUsers: []config.BasicAuthInfo{{Username: test.Username, Password: test.Password}},
-		SongBaseURL:    fileSrv.URL + "/songs/",
+		SongBaseURL:    fileSrv.URL + "/music/",
 		CoverBaseURL:   fileSrv.URL + "/covers/",
 		Presets:        presets,
 		Minify:         minify,