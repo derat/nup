derat/nup

commit 59d756ef360b3899b71849a465a4aefbf1ea3846

cmd/nup/update: Decrease import batch size and add retries.
Try to work around some App Engine flakiness that I've seen.

cmd/nup/update/server.go
@@ -20,8 +20,10 @@ import (
 )
 
 const (
-	batchSize  = 100 // updateSongs HTTP request batch size
-	tlsTimeout = time.Minute
+	batchSize        = 50 // updateSongs HTTP request batch size
+	tlsTimeout       = time.Minute
+	updateTries      = 3
+	updateRetryDelay = 3 * time.Second
 )
 
 // I started seeing "net/http: TLS handshake timeout" errors when trying to import songs.
@@ -86,7 +88,15 @@ func updateSongs(cfg *client.Config, ch chan db.Song, replaceUserData, useFilena
 	query := strings.Join(args, "&")
 
 	sendFunc := func(r io.Reader) error {
-		_, err := sendRequest(cfg, "POST", "/import", query, r, "text/plain")
+		var err error
+		for try := 1; try <= updateTries; try++ {
+			if _, err = sendRequest(cfg, "POST", "/import", query, r, "text/plain"); err == nil {
+				break
+			} else if try < updateTries {
+				log.Printf("Sleeping %v before retrying after error: %v", updateRetryDelay, err)
+				time.Sleep(updateRetryDelay)
+			}
+		}
 		return err
 	}
 

cmd/nup/update/scan.go
@@ -161,6 +161,8 @@ func scanForUpdatedSongs(cfg *client.Config, lastUpdateTime time.Time, lastUpdat
 
 		go func() {
 			// Avoid having too many parallel readSong calls, as we can run out of FDs.
+			// TODO: Find a better way to rate-limit this that also avoids creating so many
+			// simultaneous goroutines. Updating 10,000 songs brought my computer to its knees.
 			workers <- struct{}{}
 			s, err := files.ReadSong(cfg, path, fi, false, gains)
 			<-workers

commit 874a74c5b68d32b7c9eae1f9556a6cca3c15c385

cmd/nup/update: Fix empty body when retrying import.
Fix a bad bug in the retry code introduced by 59d756ef360b:
the same io.Reader was being reused for retries, so an empty
body could get sent to the server if the first attempt
failed after draining the reader. Pass the underlying
buffer's underlying byte slice instead.

Also rename updateSongs to importSongs and make it take a
bitfield of flags instead of multiple bool args, and fix an
apparent bug where the 'update' subcommand could still
update the last update timestamp and report success if
sending the songs to the server failed.

cmd/nup/update/command.go
@@ -231,8 +231,18 @@ func (cmd *Command) Execute(ctx context.Context, _ *flag.FlagSet, _ ...interface
 				return subcommands.ExitFailure
 			}
 		}
-	} else if err := updateSongs(cmd.Cfg, updateChan, replaceUserData, cmd.useFilenames); err != nil {
-		fmt.Fprintln(os.Stderr, "Failed updating songs:", err)
+	} else {
+		var flags importSongsFlag
+		if replaceUserData {
+			flags |= importReplaceUserData
+		}
+		if cmd.useFilenames {
+			flags |= importUseFilenames
+		}
+		if err := importSongs(cmd.Cfg, updateChan, flags); err != nil {
+			fmt.Fprintln(os.Stderr, "Failed updating songs:", err)
+			return subcommands.ExitFailure
+		}
 	}
 
 	if err := <-errChan; err != nil {
@@ -301,8 +311,7 @@ func (cmd *Command) doMergeSongs() subcommands.ExitStatus {
 		ch := make(chan db.Song, 1)
 		ch <- dst
 		close(ch)
-		if err := updateSongs(cmd.Cfg, ch, true, /* replaceUserData */
-			false /* useFilenames */); err != nil {
+		if err := importSongs(cmd.Cfg, ch, importReplaceUserData); err != nil {
 			fmt.Fprintf(os.Stderr, "Failed updating song %v: %v\n", dstID, err)
 			return subcommands.ExitFailure
 		}

cmd/nup/update/server.go
@@ -20,10 +20,10 @@ import (
 )
 
 const (
-	batchSize        = 50 // updateSongs HTTP request batch size
 	tlsTimeout       = time.Minute
-	updateTries      = 3
-	updateRetryDelay = 3 * time.Second
+	importBatchSize  = 50 // max songs to import per HTTP request
+	importTries      = 3
+	importRetryDelay = 3 * time.Second
 )
 
 // I started seeing "net/http: TLS handshake timeout" errors when trying to import songs.
@@ -66,35 +66,47 @@ func sendRequest(cfg *client.Config, method, path, query string,
 	return b, nil
 }
 
-// updateSongs reads all songs from ch and sends them to the server.
-//
-// replaceUserData indicates that user data (e.g. rating, tags, plays)
-// should be replaced with data from ch; otherwise the existing data is
-// preserved and only static fields (e.g. artist, title, album, etc.)
-// are replaced.
-//
-// useFilenames indicates that the server should identify songs to update
-// by their filenames rather than by SHA1s of their audio data. This can
-// be used to avoid creating a new database object after deliberately
-// modifying a song file's audio data.
-func updateSongs(cfg *client.Config, ch chan db.Song, replaceUserData, useFilenames bool) error {
+// importSongsFlag values can be masked together to configure importSongs's behavior.
+type importSongsFlag uint32
+
+const (
+	// importReplaceUserData indicates that user data (e.g. rating, tags, plays) should be
+	// replaced with data from ch; otherwise the existing data is preserved and only static
+	// fields (e.g. artist, title, album, etc.) are replaced.
+	importReplaceUserData importSongsFlag = 1 << iota
+	// importUseFilenames indicates that the server should identify songs to import by their
+	// filenames rather than by SHA1s of their audio data. This can be used to avoid creating
+	// a new database object after deliberately modifying a song file's audio data.
+	importUseFilenames
+	// importNoRetryDelay indicates that importSongs should not sleep after a failed HTTP
+	// request. This is just useful for unit tests.
+	importNoRetryDelay
+)
+
+// importSongs reads all songs from ch and sends them to the server.
+func importSongs(cfg *client.Config, ch chan db.Song, flags importSongsFlag) error {
 	var args []string
-	if replaceUserData {
+	if flags&importReplaceUserData != 0 {
 		args = append(args, "replaceUserData=1")
 	}
-	if useFilenames {
+	if flags&importUseFilenames != 0 {
 		args = append(args, "useFilenames=1")
 	}
 	query := strings.Join(args, "&")
 
-	sendFunc := func(r io.Reader) error {
+	sendFunc := func(body []byte) error {
 		var err error
-		for try := 1; try <= updateTries; try++ {
+		for try := 1; try <= importTries; try++ {
+			r := bytes.NewReader(body)
 			if _, err = sendRequest(cfg, "POST", "/import", query, r, "text/plain"); err == nil {
 				break
-			} else if try < updateTries {
-				log.Printf("Sleeping %v before retrying after error: %v", updateRetryDelay, err)
-				time.Sleep(updateRetryDelay)
+			} else if try < importTries {
+				delay := importRetryDelay
+				if flags&importNoRetryDelay != 0 {
+					delay = 0
+				}
+				log.Printf("Sleeping %v before retrying after error: %v", delay, err)
+				time.Sleep(delay)
 			}
 		}
 		return err
@@ -111,15 +123,17 @@ func updateSongs(cfg *client.Config, ch chan db.Song, replaceUserData, useFilena
 		if err := e.Encode(s); err != nil {
 			return fmt.Errorf("failed to encode song: %v", err)
 		}
-		if numSongs%batchSize == 0 {
-			if err := sendFunc(&buf); err != nil {
+		if numSongs%importBatchSize == 0 {
+			// Pass the underlying bytes rather than an io.Reader so sendFunc() can re-read the
+			// data if it needs to retry due to network issues or App Engine flakiness.
+			if err := sendFunc(buf.Bytes()); err != nil {
 				return err
 			}
 			buf.Reset()
 		}
 	}
 	if buf.Len() > 0 {
-		if err := sendFunc(&buf); err != nil {
+		if err := sendFunc(buf.Bytes()); err != nil {
 			return err
 		}
 	}

cmd/nup/update/server_test.go
@@ -16,14 +16,21 @@ import (
 	"github.com/derat/nup/test"
 )
 
-func TestUpdate(t *testing.T) {
+func TestImportSongs(t *testing.T) {
+	var numReqs int
 	recv := make([]db.Song, 0)
 	replace := ""
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		replace = r.FormValue("replaceUserData")
-
 		defer r.Body.Close()
+
+		// Make the first request fail to test the retry logic.
+		if numReqs++; numReqs == 1 {
+			http.Error(w, "Intentional failure", http.StatusInternalServerError)
+			return
+		}
+
 		d := json.NewDecoder(r.Body)
 		for {
 			s := db.Song{}
@@ -77,11 +84,11 @@ func TestUpdate(t *testing.T) {
 		ch <- s1
 		close(ch)
 	}()
-	if err := updateSongs(cfg, ch, true, false); err != nil {
+	if err := importSongs(cfg, ch, importReplaceUserData|importNoRetryDelay); err != nil {
 		t.Fatalf("Failed to send songs: %v", err)
 	}
 	if err := test.CompareSongs([]db.Song{s0, s1}, recv, test.CompareOrder); err != nil {
-		t.Error("Bad songs after initial update: ", err)
+		t.Error("Bad songs after initial import: ", err)
 	}
 	if replace != "1" {
 		t.Errorf("replaceUserData param was %q instead of 1", replace)
@@ -97,11 +104,11 @@ func TestUpdate(t *testing.T) {
 		}
 		close(ch)
 	}()
-	if err := updateSongs(cfg, ch, false, false); err != nil {
+	if err := importSongs(cfg, ch, importNoRetryDelay); err != nil {
 		t.Fatalf("Failed to send songs: %v", err)
 	}
 	if err := test.CompareSongs(sent, recv, test.CompareOrder); err != nil {
-		t.Error("Bad songs after second update: ", err)
+		t.Error("Bad songs after second import: ", err)
 	}
 	if len(replace) > 0 {
 		t.Errorf("replaceUserData param was %q instead of empty", replace)